5e8eca8f2aabc6b7686bce294d72d43cda85997e
Larry Cessna <larrybcessna@gmail.com>
2020-11-21 14:36:44 -0800

    More infrastructure files and folders. Plus removing old ones.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Employee Directory.
-        </p>
-          <p>I still need to Learn React
-        </p>
-      </header>
-    </div>
-  );
+  return 
+  <div className="App">
+     
+  <Employees/>
+  </div>
 }
 
 export default App;
